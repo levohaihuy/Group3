546a26c31a54ae92c19f998db8980b677c7f47b7
levohaihuy <levohaihuy@gmail.com>
2024-02-18 21:22:49 +0700

    Group 3

---
+++ MyFirstApp/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string username , string password)
+        {
+            if (username == "huy" && password == "123" )
+            {
+                return RedirectToAction("Dashboard","Admin");
+            }
+            else
+            {
+                return RedirectToAction("Invalidlogin");
+            }
+        }
+        public ActionResult Invalidlogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult GetEmpName(int EmpID)
+        {
+            var employess = new[]
+            {
+                new {EmpID = 1 , EmpName = "Huy" , salary = 7000},
+                new {EmpID = 2 , EmpName = "trung" , salary = 7000},
+                new {EmpID = 3 , EmpName = "Hieu" , salary = 7000},
+            };
+            string matchEmpName = null;
+            int matchsalary = 0;
+            foreach (var item in employess)
+            {
+               if(item.EmpID == EmpID)
+                {
+                    matchEmpName = item.EmpName;
+                    matchsalary = item.salary;
+                }    
+            }
+            // return new ContentResult() { Content = matchEmpName, ContentType = "text/plain" };
+            return Content(matchEmpName + " lương là : " + matchsalary);
+           
+        }
+        public ActionResult GetSalary(int EmpID)
+        {
+            string FileName = "~/bang-luong" + EmpID + ".pdf";
+            return File(FileName, "application/pdf");
+        }
+        public ActionResult FbPage(int EmpID)
+        {
+            var employess = new[]
+           {
+                new {EmpID = 1 , EmpName = "Huy" , salary = 7000},
+                new {EmpID = 2 , EmpName = "trung" , salary = 7000},
+                new {EmpID = 3 , EmpName = "Hieu" , salary = 7000},
+            };
+            string youtube = null; 
+            foreach (var item in employess)
+            {
+                if(item.EmpID == EmpID)
+                {
+                    youtube = "https://www.youtube.com/watch?v=UTJUXlHb17g" + EmpID;
+                }
+            }
+            if(youtube == null)
+            {
+                return Content("Invalid emp ID");
+            }
+            else
+            {
+                return Redirect(youtube);
+            }
+        }
+        public ActionResult empDetail()
+        {
+            ViewBag.EmpID = 179;
+            ViewBag.EmpName = "Huy";
+            ViewBag.Salary = 7000;
+            return View();
+        }
+    }
+}
+    
